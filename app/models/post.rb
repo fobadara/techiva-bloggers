e75c0f54900e39f75517f3a71641d79c1aea1dca
Abdulfatai Badara <fobadara@gmail.com>
2022-05-14 14:23:44 +0100

    Create validation and specs

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,11 @@ class Post < ApplicationRecord
   has_many :likes
   belongs_to :author, class_name: 'User', counter_cache: true
 
+  validates :title, presence: true
+  validates :title, length: { maximum: 250 }
+  validates :comments_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
